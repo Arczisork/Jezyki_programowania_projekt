859f19cfd2ff0f72abd8159e813b2f47bce25711
Arczisork <arczisork@gmail.com>
2026-01-30 17:04:50 +0100

    Zmiana struktury projektu

--- app/page.js
+++ app/page.js
@@ -24,7 +24,7 @@ export default function HomePage() {
       <main className={classes.main}>
         <section className={classes.section}>
           <h2>Sprawdź jak to działa</h2>
-          <p>Doświadczyliśmy "czystego smaku". Test kolacji w ciemności
+          <p>Doświadczyliśmy czystego smaku. Test kolacji w ciemności
 Pierwsza reakcja - nieopanowany śmiech. Czyżby stres? Może za dużo powiedziane, ale dreszczyk emocji na pewno. Mam chwycić kelnerkę uzbrojoną w noktowizor za ramię, moja towarzyszka ma chwycić za moje i tak „gęsiego” wejdziemy na salę, przebijając się przez kompletną ciemność.</p>
         </section>
 
@@ -36,7 +36,7 @@ Zaczyna się osobliwy spektakl. Muzyka na żywo, wjeżdżają przystawki. Orient
 
 Dwie rzeczy uderzają mnie najbardziej. Pierwsza: przemożna chęć jedzenia palcami. Niewprawne posługiwanie się widelcem powoduje, że nie wiem jakiej wielkości kęsy wkładam sobie do ust.
 
-Druga: brak orientacji przestrzennej. Na końcu kolacji, kiedy spada zasłona ciemności jestem zaskoczona, że pomieszczenie jest 3 razy mniejsze niż mi się wydawało, układ stolików inny niż sobie wyobrażałam. Nawet wielkość talerza wydaje mi się abstrakcyjna. I tu pojawia się kolejne pragnienie: "ach, mieć tak zmysł echolokacji"!</p>
+Druga: brak orientacji przestrzennej. Na końcu kolacji, kiedy spada zasłona ciemności jestem zaskoczona, że pomieszczenie jest 3 razy mniejsze niż mi się wydawało, układ stolików inny niż sobie wyobrażałam. Nawet wielkość talerza wydaje mi się abstrakcyjna. I tu pojawia się kolejne pragnienie: ach, mieć tak zmysł echolokacji!</p>
         </section>
       </main>
     </>
